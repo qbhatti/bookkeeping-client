859e0624eeaf46d24aa807decc6d18e20b446b15
qbhatti <qasimbhatti@hotmail.com>
2020-07-14 06:06:58 -0700

    Add form fields to AddAccountDialog

--- src/components/forms/AddAccountDialog.js
+++ src/components/forms/AddAccountDialog.js
@@ -1,38 +1,103 @@
 import React from "react";
+import useInputState from "../../hooks/useInputState";
 import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
 import ResponsiveDialog from "../ui/ResponsiveDialog";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
 
 export default function AddAccountDialog({ open, handleClose }) {
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [name, setName, resetName] = useInputState("");
+  const [email, setEmail, resetEmail] = useInputState("");
+  const [phoneNum, setPhoneNum, resetPhoneNum] = useInputState("");
+  const [category, setCategory, resetCategory] = useInputState("Individual");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`${name}, ${email}, ${phoneNum}`);
+  };
 
   const dialogTitle = (
-    <DialogTitle id="responsive-dialog-title">
-      {"Use Google's location service?"}
-    </DialogTitle>
+    <DialogTitle id="responsive-dialog-title">{"Add New Customer"}</DialogTitle>
   );
 
   const dialogContent = (
-    <DialogContentText>
-      Let Google help apps determine location. This means sending anonymous
-      location data to Google, even when no apps are running.
-    </DialogContentText>
+    <form onSubmit={handleSubmit} id="account-form">
+      <TextField
+        id="name"
+        name="name"
+        type="text"
+        label="Name"
+        margin="normal"
+        value={name}
+        onChange={setName}
+        fullWidth
+        required
+      />
+      <TextField
+        id="email"
+        name="email"
+        type="email"
+        label="Email"
+        margin="normal"
+        value={email}
+        onChange={setEmail}
+        fullWidth
+        required
+      />
+      <TextField
+        id="phoneNum"
+        name="phoneNum"
+        type="number"
+        label="Phone Number"
+        margin="normal"
+        value={phoneNum}
+        onChange={setPhoneNum}
+        fullWidth
+        required
+      ></TextField>
+
+      <TextField
+        select
+        label="Category"
+        value={category}
+        onChange={setCategory}
+        SelectProps={{
+          native: true
+        }}
+        helperText="Please choose the category of account."
+        fullWidth
+        margin="normal"
+        required
+      >
+        <option value="Customer">Individual</option>
+        <option value="Vendor">Business</option>
+        <option value="Expense">Expense</option>
+        <option value="Shareholder">Shareholder</option>
+        <option value="Inventory">Inventory</option>
+        <option value="Income">Income</option>
+        <option value="General">General</option>
+      </TextField>
+    </form>
   );
 
   const dialogActions = (
     <>
-      <Button autoFocus onClick={handleClose} color="primary">
-        Disagree
+      <Button onClick={handleClose} color="primary">
+        Cancel
       </Button>
-      <Button onClick={handleClose} color="primary" autoFocus>
-        Agree
+      <Button
+        onClick={handleSubmit}
+        color="primary"
+        form="account-form"
+        type="submit"
+        autoFocus
+      >
+        Save
       </Button>
     </>
   );
